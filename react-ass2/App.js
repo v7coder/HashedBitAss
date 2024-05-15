090eb3dcec3b2143ec4836565074acb7fa5a1587
Vinayak porje <108474353+v7coder@users.noreply.github.com>
2024-05-15 10:37:53 +0530

    Add files via upload

---
+++ react-ass2/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import RegistrationForm from './RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Registration Form</h1>
+        <RegistrationForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
